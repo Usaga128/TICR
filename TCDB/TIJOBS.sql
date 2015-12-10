80a169d8db250dbf9302137cb204d4eee9facd5b
Usaga128 <estebanusaga@hotmail.com>
2015-12-09 22:25:01 -0600

    Se agrega el proyecto con la tablad de usuarios y de anuncios.

---
+++ TCDB/TIJOBS.sql
@@ -0,0 +1,40 @@
+﻿/*
+The database must have a MEMORY_OPTIMIZED_DATA filegroup
+before the memory optimized object can be created.
+
+The bucket count should be set to about two times the 
+maximum expected number of distinct values in the 
+index key, rounded up to the nearest power of two.
+*/
+
+CREATE TABLE [dbo].[TIJOBS]
+(
+	[Id] INT NOT NULL PRIMARY KEY NONCLUSTERED HASH WITH (BUCKET_COUNT = 131072), 
+    [Title] VARCHAR(150) NULL, 
+    [Description] VARCHAR(250) NULL, 
+    [InterviewLocation] VARCHAR(100) NULL, 
+    [ExpirationDate] DATETIME NULL, 
+    [CompanyId] INT NULL, 
+    [UseSameIntJobLocation] CHAR(10) NULL, 
+    [UserId] INT NULL, 
+    [CreatedDate] DATETIME NULL, 
+    [SalaryId] INT NULL, 
+    [PlusMatrix] VARCHAR(500) NULL, 
+    [ApplyCount] INT NULL, 
+    [ExperienceId] INT NULL, 
+    [ShowCompanyName] CHAR(10) NULL, 
+    [LanguageMatrix] VARCHAR(200) NULL, 
+    [AcademyGrade] INT NULL, 
+    [CouponId] INT NULL, 
+    [PaymentId] INT NULL, 
+    [RenewCount] INT NULL, 
+    [LocationMatrix] VARCHAR(250) NULL, 
+    [ViewsCount] INT NULL, 
+    [TravelProblems] VARCHAR(50) NULL, 
+    [VacantsCount] INT NULL, 
+    [ContractId] INT NULL, 
+    [CompanyEnsurange] CHAR(10) NULL, 
+    [Teleworking] CHAR(10) NULL, 
+    [HiringCompanyId] INT NULL, 
+    [TecnologyMatrix] VARCHAR(1000) NULL
+) WITH (MEMORY_OPTIMIZED = ON)
---
+++ TCDB/TIUSERS.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [dbo].[TIUSERS]
+(
+	[UserId] INT NOT NULL PRIMARY KEY, 
+    [Profile] VARCHAR(50) NULL, 
+    [LastName] VARCHAR(100) NULL, 
+    [Surname] NVARCHAR(100) NULL, 
+    [FirstName] VARCHAR(100) NULL, 
+    [AuthMethod] VARCHAR(50) NULL, 
+    [CreatedDate] DATETIME NULL, 
+    [IsDataActive] CHAR(10) NULL, 
+    [IdType] VARCHAR(50) NULL, 
+    [Id] VARCHAR(100) NULL, 
+    [LastPasswordModDate] DATETIME NULL, 
+    [IPAddress] VARCHAR(50) NULL, 
+    [Email] VARCHAR(100) NULL, 
+    [AddToNews] NCHAR(10) NULL, 
+    [UserName] VARCHAR(100) NULL,
+	[PasswordChangeCount] INT NULL, 
+    [Password] VARCHAR(50) NULL, 
+    [OptEspecializationId] INT NULL, 
+    [JobsRequestCount] INT NULL, 
+    [CellPhoneCountry] VARCHAR(50) NULL, 
+    [CellPhone] VARCHAR(50) NULL
+)
